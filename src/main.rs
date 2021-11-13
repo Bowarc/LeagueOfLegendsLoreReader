2b20de47d66f83762a57e991b76b64af7734c996
Bowarc <yuyeaaaahhhh@laposte.net>
2021-11-13 17:13:32 +0100

    Reqwest working fine, able to get the lore of the desired champion in the desired language

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 #![allow(non_snake_case)]
-#![warn(unused_variables)]
-use std::collections::HashMap;
+#![deny(unused_variables)]
 
-const URL : &str = "https://ddragon.leagueoflegends.com/cdn/10.3.1/data/en_EN/champion/LeeSin.json";
+use serde_json::Value; //Result
+
+const DDRAGON_URL : &str = "https://ddragon.leagueoflegends.com/cdn/10.3.1/data/%LANGUAGE%/champion/%CHAMPION%.json";
+
+async fn getURL(url: String) -> Value{
+    let r: reqwest::Response = reqwest::get(url).await.expect(&format!("Couldn't get a response for the given url: \n {}.", DDRAGON_URL));
+
+    let response_text: String = r.text().await.expect(&format!("Couldn't get the text from the response."));
+    
+    let j: Value = serde_json::from_str(&response_text).expect(&format!("Couldn't create a json object form the response's text."));
+    
+    j
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
+    let champion: &str = "LeeSin";
+    let language: &str = "fr_FR";
 
-    let client = reqwest::Client::builder()
-        .build()?;
+    let url: String = DDRAGON_URL.replace("%CHAMPION%", champion)
+                        .replace("%LANGUAGE%", language);
 
-    let res = client
-        .get(URL)
-        .send()
-        .await?;
+    let LeeJsonObject: Value = getURL(url).await;
 
-    // ?  
-    let ip = res
-        .json::<HashMap<String, String>>()
-        .await?;
-    
-
-    println!("{:?}", ip);
-
-    Ok(())
+    println!("{}", LeeJsonObject["data"]["LeeSin"]["lore"].to_string());
 }
